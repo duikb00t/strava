6fe3e3c9270e70fcc862690a4db3d131b44a45c8
Ward Kennes LDL <ward@ldl.be>
2017-03-08 12:08:37 +0100

    First commit

---
+++ src/StravaServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace duikb00t\strava;
+
+use Illuminate\Support\ServiceProvider;
+
+class StravaServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        require __DIR__.'/routes/routes.php';
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ src/routes/routes.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('strava', function() {
+	return 'Strava Package got called';
+});
